6e1c67310e60d09d9e426632c0fca0ddfa7fd345
Deborags <112193794+Deborags@users.noreply.github.com>
2024-09-26 19:02:58 -0300

    atualizacoes

--- main.py
+++ main.py
@@ -7,22 +7,28 @@ class main:
     _readCliente: readCliente.read
     _createCliente: createCliente.create
 
-def __init__(self):
-    _readCliente = readCliente.read()
-    _createCliente = createCliente.create()
+    def __init__(self):
+        self._readCliente = readCliente.read()
+        self._createCliente = createCliente.create()
+    
+    def render(self):
+        st.sidebar.title("Menu")
+        page_cliente = st.sidebar.selectbox("Escolha uma opção abaixo:", [
+            "Home", "Incluir", "Consultar"])
 
-    st.sidebar.title("Menu")
-    page_cliente = st.sidebar.selectbox("Escolha uma opção abaixo:", [
-        "Home", "Incluir", "Consultar"])
+        if page_cliente == "Home":
+            st.title("Bem-vindo")
+            st.text("Olá, tudo bem? \nSelecione alguma opção no menu ao lado para"
+                    " interagir com o nosso projeto")
 
-    if page_cliente == "Home":
-        st.title("Bem vindo")
-        st.text("Olá, tudo bem? \n Selecione alguma opção no menu ao lado para"
-                "interagir com o nosso projeto")
+        elif page_cliente == "Incluir":
+            self._createCliente.cadastrar()
 
-    if page_cliente == "Incluir":
-        st.query_params.clear()
-        _createCliente.cadastrar()
+        elif page_cliente == "Consultar":
+            self._readCliente.listarClientes()
 
-    if page_cliente == "Consultar":
-        _readCliente.listarClientes()
+
+if 'main_instance' not in st.session_state:
+    st.session_state.main_instance = main()
+
+st.session_state.main_instance.render()
